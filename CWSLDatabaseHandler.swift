c6140aced3289b2ed20c98cce1c24fc19a0b5e2f
Kentaro Kawai <kawai.kentaro@gmail.com>
2023-01-10 20:01:08 +0900

    Format code

--- CWSLDatabaseHandler.swift
+++ CWSLDatabaseHandler.swift
@@ -9,22 +9,23 @@ import Foundation
 import FirebaseDatabase
 
 class CWSLDatabaseHandler {
-    private var handles = [DatabaseHandle]()
+  private var handles = [DatabaseHandle]()
 
-    deinit {
-      for handle in self.handles {
-        Database.database().reference().removeObserver(withHandle: handle)
-      }
+  deinit {
+    for handle in self.handles {
+      Database.database().reference().removeObserver(withHandle: handle)
     }
+  }
 
-    func attach(path: String,
-                event: DataEventType,
-                with: @escaping ((DataSnapshot) -> Void),
-                withCancel: ((Error) -> Void)? = nil) {
-        let db = Database.database().reference()
-        let handle = db.child(path).observe(event,
-                                            with: with,
-                                            withCancel: withCancel)
-        self.handles.append(handle)
-    }
+  func attach(path: String,
+              event: DataEventType,
+              with: @escaping ((DataSnapshot) -> Void),
+              withCancel: ((Error) -> Void)? = nil) {
+    let ref = Database.database().reference()
+    let handle = ref.child(path).observe(event,
+                                         with: with,
+                                         withCancel: withCancel)
+    self.handles.append(handle)
+  }
+
 }
